ef65b974b0b6915094d7e2ecf717464ac9b8e3ec
hp <hp@DESKTOP-K8540DJ>
2023-01-10 22:53:16 +0530

    commited

---
+++ My_Maven/src/main/java/encapsulationsample/CarDetails2.java
@@ -0,0 +1,18 @@
+package encapsulationsample;
+
+import encapsulation.CarDetails;
+
+public class CarDetails2 
+{
+	
+
+	public static void main(String[] args)
+	{
+		CarDetails ob=new CarDetails();
+		ob.setter("SELTOS","XLine",2000000,2022);
+		ob.getter();
+		
+
+	}
+
+}
