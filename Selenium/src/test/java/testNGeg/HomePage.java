da1258082254d6c1718d58fd5f6c3cfdcf682185
hp <hp@DESKTOP-K8540DJ>
2023-02-05 22:28:05 +0530

    testNg program

---
+++ Selenium/src/test/java/testNGeg/HomePage.java
@@ -0,0 +1,14 @@
+package testNGeg;
+
+import org.testng.annotations.Test;
+
+public class HomePage {
+  @Test
+  public void start() {
+	  System.out.println("Start");
+  }
+  @Test
+  public void home() {
+	  System.out.println("Home");
+  }
+}
